b26b5ce7bc4e9cd86797f362646057f5f82aec24
Jaspreet <jaspresingh@suncor.com>
2023-05-03 18:15:20 +0530

    Implemented the modules for dependency.

--- app/src/main/java/com/example/spotifyclone/di/AppModule.kt
+++ app/src/main/java/com/example/spotifyclone/di/AppModule.kt
@@ -1,10 +1,30 @@
 package com.example.spotifyclone.di
 
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.example.spotifyclone.R
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_launcher_foreground)
+            .error(R.drawable.ic_launcher_background)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+
 }
