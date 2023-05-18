0822be2aef3df1bd32ec1cc9251662351925d4e5
Jaspreet <jaspresingh@suncor.com>
2023-05-18 18:20:29 +0530

    Implemented the Notification Manager and did the necessary handlings.

--- app/src/main/java/com/example/spotifyclone/other/Constants.kt
+++ app/src/main/java/com/example/spotifyclone/other/Constants.kt
@@ -3,4 +3,6 @@ package com.example.spotifyclone.other
 object Constants {
 
     const val SONGS_COLLECTION = "songs"
+    const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_CHANNEL_ID = "music"
 }
